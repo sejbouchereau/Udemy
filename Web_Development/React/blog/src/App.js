1b72754badec18bdd8157ac8246a8e8ad2ebc179
sejbouchereau <sejbouchereau@gmail.com>
2025-03-27 19:01:51 -0400

    React test

--- Web_Development/React/blog/src/App.js
+++ Web_Development/React/blog/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const title = 'Welcome to my blog';
+  const likes = 50;
+  const google = "https://www.google.ca"
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="content">
+        <h1>{title}</h1>
+        <p>Liked {likes} times</p>
+        <p>{[1, 2, 3, 4, 5, 6, 7, 8, 9, 0]}</p>
+        <p>{Math.random()}</p>
+        <a href={google}>Google site</a>
+      </div>
     </div>
   );
 }
